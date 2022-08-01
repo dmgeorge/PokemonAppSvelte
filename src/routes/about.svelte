478ef77becaa2e5e1d2541d09ade48becf1b10da
George <georgeandreoudmg@gmail.com>
2022-08-01 17:18:31 +0300

    First Commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+    <title>About Svelte Pokedex</title>
+</svelte:head>
+<h1>About</h1>
+<p>This is a Svelte Kit Demo</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+    console.log('hi')
+</script>
+<svelte:head>
+    <title>Svelte Pokedex</title>
+</svelte:head>
+<h1>Svelte kit Pokedex</h1>
+<style>
+    h1 {
+        color: rgb(141, 35, 35);
+    }
+</style>
